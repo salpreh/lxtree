b4775f66ecb5577139cf5140caa05f14fc73712d
salpreh <salva.perez46@gmail.com>
2019-04-14 04:06:12 +0200

    Basic Tree class abstraction added

---
+++ lxtree/TreeNode.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+from .TreeChars import TreeChars
+from functools import reduce
+
+
+class TreeNode(object):
+    """
+    Basic class to generate and print data trees.
+
+    Attributes:
+        name (str): Name of thee node
+        children(lxtree.TreeNode list): Children of this node. By default `None`
+    """
+
+    def __init__(self, name='', children=None):
+        self.name = name
+        self.children = children if children else []
+
+    def __getitem__(self, i):
+        return self.children[i]
+
+    def __str__(self):
+        return self.get_three()
+
+    def insert_child(self, node, index=0):
+        """
+        Insert the node into given position.
+
+        Args:
+            node (lxtree.TreeNode): Node to insert in the child list.
+            index (int): Position where the child node will be inserted.
+                By default `0`.
+
+        Raises:
+            TypeError: If `node` argument is not an instance of `lxtree.TreeNode`.
+        """
+        if not isinstance(node, TreeNode):
+            raise TypeError('Children of a TreeNode must be also TreeNode objects')
+
+        self._children.insert(node, index)
+
+        return self
+
+    def append_child(self, node):
+        """
+        Appends the node at the end of current children list.
+
+        Args:
+            node (lxtree.TreeNode): Node to append to children list.
+
+        Raises:
+            TypeError: If `node` argument is not an instance of `lxtree.TreeNode`.
+        """
+        if not isinstance(node, TreeNode):
+            raise TypeError('Children of a TreeNode must be also TreeNode objects')
+
+        self._children.append(node)
+
+        return self
+
+    def set_children(self, *new_children):
+        """
+        Set current node children list.
+
+        Raises:
+            TypeError: If any new child argument is not an instance of `lxtree.TreeNode`.
+        """
+        self.children = new_children
+
+        return self
+
+    @property
+    def children(self):
+        """
+        Children list of the node.
+
+        Raises:
+            TypeError: If children list is reassigned and any of it's content
+                objects is not an instance of `lxtree.TreeNode`.
+        """
+        return self._children
+
+    @children.setter
+    def children(self, new_children):
+        isTNode = lambda n: isinstance(n, TreeNode)
+        allTrue = lambda a, b: a and b
+        if not reduce(allTrue, map(isTNode, new_children)):
+            raise TypeError('Children of a TreeNode must be also TreeNode objects')
+
+        self._children = new_children
+
+    def _tree_lines(self):
+        """
+        Generate the tree and returns it in a string list,
+            each item is a tree line.
+        """
+        lines = [self.name]
+        for i, child in enumerate(self.children, start=1):
+            child_lines = child._tree_lines()
+            if i < len(self.children):
+                lines.append(f'{TreeChars.VERT_RIGHT}{TreeChars.HORZ_LINE}{child_lines[0]}')
+                treeChar = f'{TreeChars.VERT_LINE} '
+
+            else:
+                lines.append(f'{TreeChars.DOWN_RIGHT}{TreeChars.HORZ_LINE}{child_lines[0]}')
+                treeChar = '  '
+
+            for child_line in child_lines[1:]:
+                lines.append(f'{treeChar}{child_line}')
+
+        return lines
+
+    def get_three(self):
+        """
+        Generates a draw of the tree content.
+
+        Returns:
+            str: Tree figure.
+        """
+        return '\n'.join(self._tree_lines())
